443c1846eb51041184df6b81502f9c04f4809f30
murilosrodrigues <murilorodrigues29@gmail.com>
2026-01-07 14:03:38 -0300

    classificacao do heroi

---
+++ index.js
@@ -0,0 +1,32 @@
+let heroeName = "Rookie";
+let experience = 1;
+let mesage = "O Herói de nome "+heroeName+" está no nível de ";
+
+
+
+for(var xp = 0; xp <=10001; xp++)
+{
+    if(experience <= 1000)
+        console.log(mesage + "Ferro")
+    else if( experience <=2000)
+        console.log(mesage + "Bronze")
+    else if( experience <=5000)
+        console.log(mesage + "Prata")
+    else if( experience <= 7000)
+        console.log(mesage + "Gold")
+    else if(experience <= 8000)
+        console.log(mesage + "Platina")
+    else if( experience <= 9000)
+        console.log(mesage + "Ascendente")
+    else if( experience <= 10000)
+        console.log(mesage + "Imortal")                
+    else 
+     {
+             console.log(mesage + "Radiante");
+             break;
+     }  
+
+    experience += 500
+}
+
+
